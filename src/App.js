11e20f06cc6cb6947ec1825b406595725dc107e1
Jayshree Upadhyay <51030227+Jaaishri@users.noreply.github.com>
2021-09-27 07:41:07 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,1 @@
 
-import './App.css';
-import UseEffectChatsCounter from './component/chats_counter/useEffectChatsCounter';
-
-function App() {
-  return (
-    <div className="App">
-      <UseEffectChatsCounter />
-    </div>
-  );
-}
-
-export default App;
